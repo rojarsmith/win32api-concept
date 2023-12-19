381e772403e0d0863a9f91cfc25c63cb05ba25a5
Rojar Smith <rojarsmith@gmail.com>
2023-12-19 19:08:05 +0800

    feat: Object-Oriented Window

---
+++ MainWindow.hpp
@@ -0,0 +1,32 @@
+#include <windows.h>
+#include "BaseWindow.hpp"
+
+class MainWindow : public BaseWindow<MainWindow>
+{
+public:
+    PCWSTR  ClassName() const { return L"Sample Window Class"; }
+    LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
+};
+
+LRESULT MainWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(m_hwnd, &ps);
+            FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+            EndPaint(m_hwnd, &ps);
+        }
+        return 0;
+
+    default:
+        return DefWindowProc(m_hwnd, uMsg, wParam, lParam);
+    }
+    return TRUE;
+}
---
+++ main_oop.cpp
@@ -0,0 +1,29 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+#include "MainWindow.hpp"
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    MainWindow win;
+
+    if (!win.Create(L"Learn to Program Windows", WS_OVERLAPPEDWINDOW))
+    {
+        return 0;
+    }
+
+    ShowWindow(win.Window(), nCmdShow);
+
+    // Run the message loop.
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
