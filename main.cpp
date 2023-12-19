48d619b019e91b1b7fd0b96522cb93c04af8c13c
Rojar Smith <rojarsmith@gmail.com>
2023-12-19 09:44:37 +0800

    feat: Prepare Window

--- main.cpp
+++ main.cpp
@@ -4,7 +4,54 @@
 
 #include <windows.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+/*
+hInstance is the handle to an instance or handle to a module. The operating system uses this value to identify the executable or EXE when it's loaded in memory. Certain Windows functions need the instance handle, for example to load icons or bitmaps.
+hPrevInstance has no meaning. It was used in 16-bit Windows, but is now always zero.
+pCmdLine contains the command-line arguments as a Unicode string.
+nCmdShow is a flag that indicates whether the main application window is minimized, maximized, or shown normally.
+*/
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
+    // Register the window class.
+    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc; // CallBack
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                   // Optional window styles.
+        CLASS_NAME,          // Window class
+        L"Sample Windows",   // Window text
+        WS_OVERLAPPEDWINDOW, // Window style
+
+        // Size and position
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,      // Parent window
+        NULL,      // Menu
+        hInstance, // Instance handle
+        NULL       // Additional application data
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+
     return 0;
 }
